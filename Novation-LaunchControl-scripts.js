0ef82630d811f69f440b7e14410b8f3d9bd61caa
Jesse <10161205+jfrench206@users.noreply.github.com>
2018-07-04 02:52:31 -0500

    Added granular brightness control

--- Novation-LaunchControl-scripts.js
+++ Novation-LaunchControl-scripts.js
@@ -4,32 +4,20 @@
 // Official programmer's reference guide to the hardware is available here and very useful:
 // https://global.novationmusic.com/launch/launch-control/support-downloads
 
-// todo:
-// refactor LC.ledColor so that global brightness can be overridden on a per message basis
-
 var LC = new Controller();
 LC.byteArray = [];
 
 // ---- User Settings: -------------------
-LC.b = 0; // global brightness - 0 is dim, 1 is medium, 2 is bright - pick the one you want
+LC.gb = 0; // global brightness - 0 is dim, 1 is medium, 2 is bright - pick the one you want
 LC.myColor = 'amber'; // preferred default color of LED to use - choices are amber, red, green
 LC.t = {u: 0x03, f: 0x09}; // template setting - user and factory
-//
+
 // Note: templates above are set to User Template 4 (index 3) and Factory Template 10 (index 9) because I don't have the 
 // ability to run the device programming app, and that's what my hardware is set to. You can use 'mixxx --mididebug' 
 // from command line to find your template settings - corresponds to incoming MIDI channel.
 //
 // ---- End User Settings ----------------
 
-
-// define an object with color settings for the Launch Control - see programmer's guide for explanation
-// incorporates global brightness setting
-LC.ledColor = {
-	off: 0x0C,
-	red: 0x0D+LC.b,
-	amber: 0x1D+LC.b+LC.b*16,
-	green: 0x1C+LC.b*16
-};
 
 // pieces of the SysEx message to set LED values
 LC.ledBytePart = [0xF0, 0x00, 0x20, 0x29, 0x02, 0x0A, 0x78]; // this part of the LED setting message does not vary
@@ -39,15 +27,15 @@ LC.led = { // this is the customizable part
 	color: '',
 	last: 0xF7,
 	on: function (n, color, brightness, temp){
-		// set some defaults if no arguments are provided
+		// set some defaults if no arguments are provided - incorporates user settings
 		n = n || 0;
 		color = color || LC.myColor;
-		brightness = brightness || LC.b;
+		brightness = brightness || LC.gb;
 		temp = temp || LC.t.u;
 
 		// set up the data
 		this.num = n;
-		this.color = LC.ledColor[color];
+		this.color = getColor(color,brightness);
 		this.template = temp;
 
 		// assemble packet and send to device
@@ -59,8 +47,17 @@ LC.led = { // this is the customizable part
 	}
 };
 
-// LED feedback for effects
-// using engine.makeConnection to bind to Mixxx controls
+// LED color settings for the Launch Control - see programmer's guide for full explanation. b is a brightness control
+function getColor(color,b){
+	switch (color) {
+		case 'off': return 0x0c;
+		case 'red': return 0x0D+b;
+		case 'amber': return 0x1D+b+b*16;
+		case 'green': return 0x1C+b*16;
+	}
+}
+
+// LED feedback for effects, using engine.makeConnection to bind to Mixxx controls
 // here 'v' is a boolean value that corresponds to the effect on / off state in Mixxx
 // the function at the end says: if the effect is enabled (v === true), then turn on the proper LED, otherwise turn it off
 
@@ -77,10 +74,10 @@ var effects7 = engine.makeConnection('[EffectRack1_EffectUnit2_Effect3]','enable
 var effects8 = engine.makeConnection('[EffectRack1_EffectUnit4_Effect1]','enabled',function (v){v ? LC.led.on(7) : LC.led.off(7)});
 
 // LED feedback for FX headphone send, and FX assign on decks
-var arrow1 = engine.makeConnection('[EffectRack1_EffectUnit1]','group_[Headphone]_enable',function (v){v ? LC.led.on(10) : LC.led.off(10)});
-var arrow2 = engine.makeConnection('[EffectRack1_EffectUnit2]','group_[Headphone]_enable',function (v){v ? LC.led.on(8) : LC.led.off(8)});
-var arrow3 = engine.makeConnection('[EffectRack1_EffectUnit1]','group_[Channel1]_enable',function (v){v ? LC.led.on(11) : LC.led.off(11)});
-var arrow4 = engine.makeConnection('[EffectRack1_EffectUnit2]','group_[Channel2]_enable',function (v){v ? LC.led.on(9) : LC.led.off(9)});;
+var arrow1 = engine.makeConnection('[EffectRack1_EffectUnit1]','group_[Headphone]_enable',function (v){v ? LC.led.on(10,'',1) : LC.led.off(10)});
+var arrow2 = engine.makeConnection('[EffectRack1_EffectUnit2]','group_[Headphone]_enable',function (v){v ? LC.led.on(8,'',1) : LC.led.off(8)});
+var arrow3 = engine.makeConnection('[EffectRack1_EffectUnit1]','group_[Channel1]_enable',function (v){v ? LC.led.on(11,'',1) : LC.led.off(11)});
+var arrow4 = engine.makeConnection('[EffectRack1_EffectUnit2]','group_[Channel2]_enable',function (v){v ? LC.led.on(9,'',1) : LC.led.off(9)});;
 
 
 LC.LEDtoArray = function () { // can't use Object.values in this environment, so this is a kludge
